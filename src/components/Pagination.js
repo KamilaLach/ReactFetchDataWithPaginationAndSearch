f02862012fb7ad702bd601f18f58be3b2c15facb
KamilaLach <55918299+KamilaLach@users.noreply.github.com>
2022-06-07 00:30:04 +0200

    small fix

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -17,20 +17,20 @@ const Pagination = props => {
         <Container maxWidth="sm">
             <ul className="pagination justify-content-center ">
                 <li className={`page-item prev ${props.currentPage === 1 ? 'disabled' : ''}`}>
-                    <a onClick={() => props.paginate(props.currentPage - 1)} className="page-link" href="!#">
+                    <a onClick={() => props.paginate(props.currentPage - 1)} className="page-link">
                         <ArrowBackIosIcon fontSize="small"/>
                     </a>
                 </li>
 
                 {pages.map(number => (
                     <li key={number} className={`page-item ${props.currentPage === number ? 'active' : null}`}>
-                        <a onClick={() => props.paginate(number)} hef="!#" className="page-link rounded-circle">
+                        <a onClick={() => props.paginate(number)} className="page-link rounded-circle">
                             {number}
                         </a>
                     </li>
                 ))}
                 <li className={`page-item next ${props.currentPage === pages.length ? 'disabled' : ''}`}>
-                    <a onClick={() => props.paginate(props.currentPage + 1)} className="page-link" href="!#">
+                    <a onClick={() => props.paginate(props.currentPage + 1)} className="page-link">
                         <ArrowForwardIosIcon fontSize="small"/>
                     </a>
                 </li>
